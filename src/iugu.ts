a30a1c5e64718cd7d2e6b649eae71d8eafbec7f4
Henrique Wilson <me@henriquewilson.com.br>
2020-07-18 16:36:55 -0300

    new methods

--- src/iugu.ts
+++ src/iugu.ts
@@ -37,6 +37,36 @@ export class Iugu {
         });
     }
 
+    charge(customerId: string, token: string, payer: any, orderId: string, price: number) {
+        let body: any = {
+            customer_id: customerId,
+            token: token,
+            email: payer.email,
+            order_id: orderId,
+            items: [
+                {
+                    description: '',
+                    quantity: 1,
+                    price_cents: price
+                }
+            ]
+        }
+
+        if (true) {
+            body['method'] = 'bank_slip';
+            body['restrict_payment_method'] = true;
+            body['bank_slip_extra_days'] = 30;
+            body['payer'] = payer;
+        }
+
+        return rp.post(`${this.subscription.baseUrl}charge`, {
+            headers: {'Content-Type': 'application/json'},
+            json: true,
+            body: body
+        });
+
+    }
+
     /**
      *
      * @param {string} apiKey
