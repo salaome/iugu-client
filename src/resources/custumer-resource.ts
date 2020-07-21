97fc533229f2d0aa30d4b2a75b74799d10eb9aff
Henrique Wilson <me@henriquewilson.com.br>
2020-07-21 17:06:24 -0300

    no message

--- src/resources/custumer-resource.ts
+++ src/resources/custumer-resource.ts
@@ -96,7 +96,7 @@ export class CustomerResource extends IuguResouces<Customer> {
                 headers: this.$getHeader()
             });
         } catch (e) {
-            return {e, url: `${this.baseUrl}invoices`};
+            return new Promise((resolve, reject) => resolve({e, url: `${this.baseUrl}invoices`}));
         }
     }
 
