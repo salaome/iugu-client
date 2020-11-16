46ffd68c321a25c9e1c931cb51a7d7f608239d23
Henrique Wilson <me@henriquewilson.com.br>
2020-11-16 13:59:18 -0300

    suspend

--- src/resources/subscription-resource.ts
+++ src/resources/subscription-resource.ts
@@ -1,5 +1,6 @@
 import {IuguResouces} from "./iugu-resouces";
 import {Subscription} from "../iugu-interfaces";
+import * as rp from "request-promise-native";
 
 export class SubscriptionResource extends IuguResouces<Subscription> {
 
@@ -7,6 +8,13 @@ export class SubscriptionResource extends IuguResouces<Subscription> {
 
    constructor(token: string | undefined) {
       super(token);
+   }
+
+   suspend(subscription_id: string): Promise<any> {
+      return rp.post(`${this.$getUri()}${subscription_id}/suspend`, {
+         json: true,
+         headers: this.$getHeader()
+      });
    }
 
 }
